a5a06d2b4d067ce899f4475f5590e61f8772485b
Ashraful Haque Sulaiman <group6bsakm@gmail.com>
2026-04-08 23:48:06 +0600

    Delete backend directory

--- backend/get-company-id.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const company = await prisma.company.findFirst();
-  console.log('COMPANY_ID:', company?.id);
-  await prisma.$disconnect();
-}
-main();
--- backend/prisma/migrations/20260225230000_add_account_cash_flow_type/migration.sql
+++
@@ -1,4 +0,0 @@
--- Migration: Add cashFlowType to Account table
--- This field was defined in schema.prisma but was missing from migrations
-
-ALTER TABLE "Account" ADD COLUMN IF NOT EXISTS "cashFlowType" TEXT;
